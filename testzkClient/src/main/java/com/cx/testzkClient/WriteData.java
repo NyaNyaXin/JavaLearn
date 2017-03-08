414da836ccc2eaf6720fd41e0d6f7309116ea2b0
NyaNyaXin <chenxin95013>
2017-03-08 18:19:05 +0800

    zkClient Using

--- testzkClient/src/main/java/com/cx/testzkClient/WriteData.java
+++ testzkClient/src/main/java/com/cx/testzkClient/WriteData.java
@@ -5,16 +5,18 @@ import org.I0Itec.zkclient.serialize.SerializableSerializer;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 
-public class GetData {
+public class WriteData {
 	public static void main(String[] args) {
 		//建立连接					        服务器的ip:port,会话过期时间 ,连接超时时间,序列化器(可以处理java对象)
 		ZkClient zc = new ZkClient("192.168.199.129:2181",10000,10000,new SerializableSerializer());
 		System.out.println("conneted ok");
-		//获取节点的状态信息
-		Stat stat = new Stat();
-		//获取节点中的数据内容  参数：节点的完整路径,stat状态对象  		这里返回的是User对象
-		User u = zc.readData("/node_1",stat);
-		System.out.println("用户:"+u.getId()+",名字："+u.getName());
-		System.out.println(stat);
+		
+		User u = new User();
+		u.setId(2);
+		u.setName("hehe");
+		
+		//更改节点数据	节点路径,节点的新的数据内容,数据版本号
+		zc.writeData("/node_1", u,-1 );
+		GetData.main(null);
 	}
 }

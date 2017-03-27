93cdcba180066c9d68fcff066e71e02386fb2e8e
NyaNyaXin <chenxin95013>
2017-03-27 16:47:02 +0800

    Spring IOC和DI概述

--- Spring01/src/com/cx/spring/beans/Main.java
+++ Spring01/src/com/cx/spring/beans/Main.java
@@ -12,9 +12,11 @@ public class Main {
 		//调用hello方法
 		
 		//1.创建Spring的IOC容器对象
+		//ApplicationContext 代表IOC容器
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//2.从IOC容器中获取Bean实例
-//		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld2");
+		System.out.println(helloWorld);
 //		//3.调用方法
 //		helloWorld.hello();
 	}

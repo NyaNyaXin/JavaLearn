8f160e5ebd9d0f8115b66113f6b9ddb6e6face0e
NyaNyaXin <chenxin95013>
2017-03-27 15:06:08 +0800

    Spring HelloWorld

---
+++ Spring01/src/com/cx/spring/beans/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.cx.spring.beans;
+
+public class HelloWorld {
+	private String name;
+
+	public void setName(String name) {
+		System.out.println("setName"+ name);
+		this.name = name;
+	}
+	
+	public void hello(){
+		System.out.println("hello:"+name);
+	}
+	
+	public HelloWorld() {
+		System.out.println("HelloWorld's Constructor....");
+	}
+}
---
+++ Spring01/src/com/cx/spring/beans/Main.java
@@ -0,0 +1,21 @@
+package com.cx.spring.beans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+//		//创建helloworld对象
+//		HelloWorld hw = new HelloWorld();
+//		//对name属性复制
+//		hw.setName("chenxin");
+		//调用hello方法
+		
+		//1.创建Spring的IOC容器对象
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//2.从IOC容器中获取Bean实例
+//		HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+//		//3.调用方法
+//		helloWorld.hello();
+	}
+}

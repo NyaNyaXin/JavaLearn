9299f4ade7b6a267acfd2a23fa1363d71fc002ef
NyaNyaXin <chenxin95013>
2017-03-28 19:31:53 +0800

    Spring Bean之间的关系之依赖bean配置

--- Spring01/src/com/cx/spring/beans/relation/Main.java
+++ Spring01/src/com/cx/spring/beans/relation/Main.java
@@ -4,8 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cx.spring.beans.autowire.Address;
+import com.cx.spring.beans.autowire.Person;
 
 public class Main {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-relation.xml");
 //		Address address = (Address) ctx.getBean("address");
@@ -13,5 +17,8 @@ public class Main {
 		
 		Address address2 = (Address) ctx.getBean("address2");
 		System.out.println(address2);
+		
+		Person person = (Person) ctx.getBean("person");
+		System.out.println(person);
 	}
 }

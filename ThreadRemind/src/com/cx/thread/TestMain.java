266aa4b9ce68d1ae2bb49589a7d51edbe7655959
NyaNyaXin <chenxin95013>
2017-03-29 18:11:19 +0800

    Java复习(多线程)之通过继承Thread类来创建多线程

---
+++ ThreadRemind/src/com/cx/thread/TestMain.java
@@ -0,0 +1,16 @@
+package com.cx.thread;
+//单线程！主线程
+public class TestMain {
+	public static void main(String[] args) {
+		method2("cx");
+	}
+	public static void method1(String str){
+		System.out.println("method1....");
+		System.out.println(str);
+	}
+	
+	public static void method2(String str){
+		System.out.println("method2.....");
+		method1(str);
+	}
+}
---
+++ ThreadRemind/src/com/cx/thread/TestThread.java
@@ -0,0 +1,33 @@
+package com.cx.thread;
+
+/**
+ * 创建一个子线程，完成1-100之间自然数得输出，同样的，也让主线程执行同样的操作
+ * 创建多线程的第一种方式：继承java.lang.Thread类
+ * */
+
+//1.创建继承Thread的子类
+class SubThread extends Thread {
+	//2.重写Thread类的run()方法,方法内实现此子线程要完成的功能
+	public void run(){
+		for(int i=1;i<=100;i++){
+			System.out.println(Thread.currentThread().getName()+":"+i);
+		}
+	}
+}
+
+public class TestThread{
+	public static void main(String[] args) {
+		//3.创建子类的对象
+		SubThread su = new SubThread();
+		//4.调用线程的start方法，启动此线程；调用响应的run()方法
+		//一个线程只能够执行一次start方法，不能通过实现类对象的run()方法去启动一个线程
+		su.start();
+		SubThread su2 = new SubThread();
+		su2.start();
+		
+		for(int i=1;i<=100;i++){
+			System.out.println(Thread.currentThread().getName()+":"+i);
+		}
+	}
+	
+}

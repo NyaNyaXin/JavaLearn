ed52fc96564db2e7b1391ca931bffd62569925d8
NyaNyaXin <chenxin95013>
2017-03-30 11:26:02 +0800

    Spring通过工厂方法配置bean之静态工厂方法

---
+++ Spring01/src/com/cx/spring/beans/factory/Main.java
@@ -0,0 +1,12 @@
+package com.cx.spring.beans.factory;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-factory.xml");
+		Car car1 = (Car) ctx.getBean("car1");
+		System.out.println(car1);
+	}
+}

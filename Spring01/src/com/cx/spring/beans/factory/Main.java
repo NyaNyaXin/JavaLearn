9a2ccd0ccdf08377af15714b5fefbffa52f71ea7
NyaNyaXin <chenxin95013>
2017-03-30 17:54:42 +0800

    Spring通过工厂方法配置bean之实例工厂方法

--- Spring01/src/com/cx/spring/beans/factory/Main.java
+++ Spring01/src/com/cx/spring/beans/factory/Main.java
@@ -8,5 +8,8 @@ public class Main {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-factory.xml");
 		Car car1 = (Car) ctx.getBean("car1");
 		System.out.println(car1);
+		
+		Car car2 = (Car) ctx.getBean("car2");
+		System.out.println(car2);
 	}
 }

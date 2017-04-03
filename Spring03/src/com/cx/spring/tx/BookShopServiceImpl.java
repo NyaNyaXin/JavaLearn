3c9844c8f9f6ca48078bdb7bea0a3c52aafe4294
NyaNyaXin <chenxin95013>
2017-04-03 12:12:23 +0800

    Spring事务之声明式事务

--- Spring03/src/com/cx/spring/tx/BookShopServiceImpl.java
+++ Spring03/src/com/cx/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.cx.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("bookShopService")
 public class BookShopServiceImpl implements BookShopService {
@@ -9,6 +10,8 @@ public class BookShopServiceImpl implements BookShopService {
 	private BookShopDao bookShopDao;
 
 	@Override
+	//添加事务注解
+	@Transactional
 	public void purchase(String username, String isbn) {
 		//1.获取书的单价
 		int price = bookShopDao.findBookPriceByIsbn(isbn);

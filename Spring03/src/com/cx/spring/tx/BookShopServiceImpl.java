2d79194cd58f93e53566b29b0a6153913dd8f20f
NyaNyaXin <chenxin95013>
2017-04-03 12:42:07 +0800

    Spring事务处理之事务的传播行为

--- Spring03/src/com/cx/spring/tx/BookShopServiceImpl.java
+++ Spring03/src/com/cx/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.cx.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service("bookShopService")
@@ -11,7 +12,13 @@ public class BookShopServiceImpl implements BookShopService {
 
 	@Override
 	//添加事务注解
-	@Transactional
+	/**
+	 * 使用propagation属性指定事务的传播行为，即当前的事务方法被另外一个事务方法调用式如何使用事务
+	 * 默认取值为REQUIRED，即使用调用方法的事务
+	 * REQUIRES_NEW使用自己的事务，调用方法本身的事务被挂起
+	 * */
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
 	public void purchase(String username, String isbn) {
 		//1.获取书的单价
 		int price = bookShopDao.findBookPriceByIsbn(isbn);
--- Spring03/src/com/cx/spring/tx/SpringTransationTest.java
+++ Spring03/src/com/cx/spring/tx/SpringTransationTest.java
@@ -2,6 +2,8 @@ package com.cx.spring.tx;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,12 +11,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringTransationTest {
 	
 	private ApplicationContext ctx = null;
-	BookShopDao bookShopDao = null;
-	BookShopService bookShopService = null;
+	private BookShopDao bookShopDao = null;
+	private BookShopService bookShopService = null;
+	private Cashier cashier = null;
 	{
 		ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		bookShopDao = ctx.getBean(BookShopDao.class);
 		bookShopService = ctx.getBean(BookShopService.class);
+		cashier = ctx.getBean(Cashier.class);
 	}
 
 	@Test
@@ -35,5 +39,8 @@ public class SpringTransationTest {
 	public void testBookShopService(){
 		bookShopService.purchase("AA", "1001");
 	}
-
+	@Test
+	public void testTransationPropagation(){
+		cashier.checkout("AA", Arrays.asList("1001","1002"));
+	}
 }

92e34e5fefe909545878aa1c0eedf874ccf53522
NyaNyaXin <chenxin95013>
2017-04-03 17:12:45 +0800

    Hibernate HelloWorld(配置文件及常用方法)

---
+++ Hibernate01/src/com/cx/hibernate/helloworld/News.java
@@ -0,0 +1,92 @@
+package com.cx.hibernate.helloworld;
+
+import java.sql.Date;
+
+public class News {
+	private Integer id;
+	private String title;
+	private String author;
+	private Date date;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * @param author
+	 *            the author to set
+	 */
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date
+	 *            the date to set
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public News(String title, String author, Date date) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.date = date;
+	}
+
+	public News() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "News [id=" + id + ", title=" + title + ", author=" + author + ", date=" + date + "]";
+	}
+
+}

0c25329ce5265c2ef22586ec3793dd38bdb96dc9
NyaNyaXin <chenxin95013>
2017-04-04 14:23:39 +0800

    Hibernate Session的核心方法之update()

--- Hibernate02/src/com/cx/hibernate/entities/HibernateTest.java
+++ Hibernate02/src/com/cx/hibernate/entities/HibernateTest.java
@@ -14,6 +14,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
 public class HibernateTest {
 
 	private SessionFactory sessionFactory;
@@ -36,36 +37,64 @@ public class HibernateTest {
 		session.close();
 		sessionFactory.close();
 	}
+
 	/**
-	 * get VS load
-	 * 1.执行get方法会立即加载对象。而执行load方法，若不使用该对象，则不会立即执行查询操作，而返回一个代理对象
-	 * 	 get是立即检索，load是延迟检索
-	 * 2.load方法可能会抛出懒加载异常LazyInitializationExecption:在需要初始化代理对像之前已经关闭了Session
-	 * 	 
-	 * 3.若数据表中没有对应的记录，Session也没有被关闭
-	 * 	 get返回null，load若不使用该对象的任何属性，没有问题；若需要初始化则抛异常
-	 * **/
+	 * update:
+	 * 1.若更新一个持久化对象，不需要显式的调用update方法，因为在调用transaction的commit方法时，会先执行session的flush方法
+	 * 2.若要更新一个游离对象，需要显式的调用session的update方法.可以把一个游离对象变为持久化对象
+	 * 
+	 * 注意: 1.无论要更新的游离对象和数据表的记录是否一致，都会发送update语句
+	 * 如何能让update方法不再盲目的触发update语句？在.hbm.xml文件的class节点设置select-before-update=true(默认为false)，
+	 * 但通常不需要设置该属性
+	 * 2.若数据表中没有对应的记录，但还调用了update方法会抛出异常
+	 * 3.当 update() 方法关联一个游离对象时, 如果在 Session 的缓存中已经存在相同 OID 的持久化对象, 会抛出异常
+	 * 因为在sessio缓存中不能有两个OID相同的对象！
+	 **/
 	@Test
-	public void testLoad(){
+	public void testUpdate() {
+		News news = (News) session.get(News.class, 1);
+
+		transaction.commit();
+		session.close();
+//		news.setId(100);
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+
+		// news.setAuthor("Sun");
+		
+		News news2 = (News) session.get(News.class, 1);
+		session.update(news);
+	}
+
+	/**
+	 * get VS load 1.执行get方法会立即加载对象。而执行load方法，若不使用该对象，则不会立即执行查询操作，而返回一个代理对象
+	 * get是立即检索，load是延迟检索
+	 * 2.load方法可能会抛出懒加载异常LazyInitializationExecption:在需要初始化代理对像之前已经关闭了Session
+	 * 
+	 * 3.若数据表中没有对应的记录，Session也没有被关闭 get返回null，load若不使用该对象的任何属性，没有问题；若需要初始化则抛异常
+	 **/
+	@Test
+	public void testLoad() {
 		News news = (News) session.load(News.class, 10);
 		System.out.println(news.getClass().getName());
-		//session.close();
-		//System.out.println(news);
+		// session.close();
+		// System.out.println(news);
 	}
+
 	@Test
-	public void testGet(){
+	public void testGet() {
 		News news = (News) session.get(News.class, 1);
-		//session.close();
+		// session.close();
 		System.out.println(news);
 	}
+
 	/**
 	 * persist()也会执行insert操作
 	 * 
-	 * 和save()方法的区别：
-	 * 在persist()方法之前，若对象已经有id了，则不会执行insert而会抛出异常
-	 * **/
+	 * 和save()方法的区别： 在persist()方法之前，若对象已经有id了，则不会执行insert而会抛出异常
+	 **/
 	@Test
-	public void testPersist(){
+	public void testPersist() {
 		News news = new News();
 		news.setTitle("EE");
 		news.setAuthor("ee");
@@ -75,40 +104,38 @@ public class HibernateTest {
 		session.persist(news);
 		System.out.println(news);
 	}
-	
+
 	/**
-	 * save()方法：
-	 * 1.是一个临时对象变为持久化对象
-	 * 2.为对象分配id
-	 * 3.在flush缓存时会发送一条insert语句
-	 * 4.在save()方法之前设置的id是无效的
-	 * 5.持久化对象的id是不能被修改的！
-	 * **/
+	 * save()方法： 1.是一个临时对象变为持久化对象 2.为对象分配id 3.在flush缓存时会发送一条insert语句
+	 * 4.在save()方法之前设置的id是无效的 5.持久化对象的id是不能被修改的！
+	 **/
 	@Test
-	public void testSave(){
+	public void testSave() {
 		News news = new News();
 		news.setTitle("CC");
 		news.setAuthor("cc");
 		news.setDate(new Date());
-		//news.setId(100);
+		// news.setId(100);
 		System.out.println(news);
 		session.save(news);
 		System.out.println(news);
-		//news.setId(101);
+		// news.setId(101);
 	}
+
 	@Test
-	public void testClear(){
+	public void testClear() {
 		News news = (News) session.get(News.class, 1);
 		session.clear();
 		News news2 = (News) session.get(News.class, 1);
 		System.out.println(news);
 	}
+
 	/**
 	 * refresh()：会强制发送SELECT语句，以使Session缓存中对象的状态和数据表中对应的记录保持一致！
-	 * **/
+	 **/
 	@Test
-	public void testRefresh(){
-		News news = (News) session.get(News.class,1);
+	public void testRefresh() {
+		News news = (News) session.get(News.class, 1);
 		System.out.println(news);
 		session.refresh(news);
 		System.out.println(news);
@@ -121,23 +148,24 @@ public class HibernateTest {
 	 * 1).执行HQL或QBC查询，会先进行flush()操纵,以得到数据表的最新的数据
 	 * 2).若记录的id是由底层数据库使用自增的方式生成的，则在调用save方法时就会立即发送insert语句，因为save方法后，必须保证对象的id
 	 * 是存在的！
-	 * **/
-	
+	 **/
+
 	@Test
-	public void testSessionFlush2(){
-		News news = new News("Java","SUN",new Date());
+	public void testSessionFlush2() {
+		News news = new News("Java", "SUN", new Date());
 		session.save(news);
 	}
+
 	@Test
-	public void testSessionFlush(){
-		 News news = (News) session.get(News.class, 1);
-		 news.setAuthor("Oracle");
-		 
-//		 session.flush();
-//		 System.out.println("flush");
-		 
-		 News news2 = (News) session.createCriteria(News.class).uniqueResult();
-		 System.out.println(news2);
+	public void testSessionFlush() {
+		News news = (News) session.get(News.class, 1);
+		news.setAuthor("Oracle");
+
+		// session.flush();
+		// System.out.println("flush");
+
+		News news2 = (News) session.createCriteria(News.class).uniqueResult();
+		System.out.println(news2);
 	}
 
 	@Test

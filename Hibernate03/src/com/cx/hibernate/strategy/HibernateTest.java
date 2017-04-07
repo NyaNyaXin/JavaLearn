0d53f68588b2ab8da7d90bd30e5d63f6c5cfe660
NyaNyaXin <chenxin95013>
2017-04-07 14:21:45 +0800

    Hibernate检索策略值一对多和多对多的检索策略

--- Hibernate03/src/com/cx/hibernate/strategy/HibernateTest.java
+++ Hibernate03/src/com/cx/hibernate/strategy/HibernateTest.java
@@ -9,6 +9,7 @@ import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
 
 import org.hibernate.Hibernate;
 import org.hibernate.Session;
@@ -45,6 +46,63 @@ public class HibernateTest {
 		transaction.commit();
 		session.close();
 		sessionFactory.close();
+	}
+	@Test
+	public void testSetFetch2(){
+		Customer customer = (Customer) session.get(Customer.class, 1);
+		System.out.println(customer.getOrders().size());
+	
+	}
+	@Test
+	public void testSetFetch(){
+		List<Customer> customers = session.createQuery("FROM Customer").list();
+		System.out.println(customers.size());
+		
+		for(Customer customer:customers)
+		{
+			if(customer.getOrders()!=null){
+				System.out.println(customer.getOrders().size());
+			}
+		}
+		//----------------------------Set集合的Fetch属性-------------------------
+		//确定初始化orders集合的方式
+		//1.默认值为select。通过正常的方式来初始化set元素
+		//2.可以取值为subselect。通过子查询的方式来初始化所有的set集合，此时lazy有效但batch-size失效
+		//子查询作为where子句的in的条件出现
+		//子查询查询所有1的一端的id
+		//3若取值为join则
+		//3.1在加载1的一端对象时，使用迫切左外连接（使用左外连接进行查询且把集合属性进行初始化）的方式检索n的一端的集合属性
+		//3.2忽略lazy属性
+		//3.3HQL查询忽略fetch=join的取值
+	}
+	@Test
+	public void testSetBatchSize(){
+		List<Customer> customers = session.createQuery("FROM Customer").list();
+		System.out.println(customers.size());
+		
+		for(Customer customer:customers)
+		{
+			if(customer.getOrders()!=null){
+				System.out.println(customer.getOrders().size());
+			}
+		}
+		//-----------------------set元素的batch-size属性------------------------------
+		//设置一次初始化set集合的数量
+	}
+	@Test
+	public void testOneToManyLevelStrategy(){
+		Customer customer = (Customer) session.get(Customer.class, 1);
+		System.out.println(customer.getCustomerName());
+		
+		Hibernate.initialize(customer.getOrders());
+		//------------------------set的lazy属性-------------------------------------
+		//1.一对多或多对多的集合属性，默认使用懒加载检索策略
+		//2.可以通过设置set的lazy属性来修改默认的检索策略,默认为true，并不建议设置为false
+		//3.lazy还可以设置为extra，增强的延迟检索,该取值会尽可能的延迟集合初始化的时机
+		Order order = new Order();
+		order.setOrderId(1);
+		System.out.println(customer.getOrders().size());
+		System.out.println(customer.getOrders().contains(order));
 	}
 	@Test
 	public void testClassLevelStrategy(){
--- Hibernate03/src/com/cx/hibernate/strategy/Order.java
+++ Hibernate03/src/com/cx/hibernate/strategy/Order.java
@@ -47,5 +47,37 @@ public class Order {
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
+
+	@Override
+	public String toString() {
+		return "Order [orderId=" + orderId + ", orderName=" + orderName + ", customer=" + customer + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		return true;
+	}
+	
+	
 	
 }

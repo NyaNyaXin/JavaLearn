88823d71e41520d3394597514231518a998f9893
NyaNyaXin <chenxin95013>
2017-04-07 13:40:16 +0800

    Hibernate检索策略之类级别的检索策略

---
+++ Hibernate03/src/com/cx/hibernate/strategy/Customer.java
@@ -0,0 +1,51 @@
+package com.cx.hibernate.strategy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Customer {
+	private Integer customerId;
+	private String customerName;
+	
+	private Set<Order> orders = new HashSet<>();
+	
+	
+	/**
+	 * @return the orders
+	 */
+	public Set<Order> getOrders() {
+		return orders;
+	}
+	/**
+	 * @param orders the orders to set
+	 */
+	public void setOrders(Set<Order> orders) {
+		this.orders = orders;
+	}
+	/**
+	 * @return the customerId
+	 */
+	public Integer getCustomerId() {
+		return customerId;
+	}
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+	/**
+	 * @return the customerName
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+	/**
+	 * @param customerName the customerName to set
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	
+}
---
+++ Hibernate03/src/com/cx/hibernate/strategy/HibernateTest.java
@@ -0,0 +1,56 @@
+package com.cx.hibernate.strategy;
+
+import static org.junit.Assert.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.Work;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+public class HibernateTest {
+
+	private SessionFactory sessionFactory;
+	private Session session;
+	private Transaction transaction;
+
+	@Before
+	public void init() {
+		Configuration configuration = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties())
+				.buildServiceRegistry();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+	}
+
+	@After
+	public void destory() {
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+	@Test
+	public void testClassLevelStrategy(){
+		Customer customer = (Customer) session.load(Customer.class, 1);
+		System.out.println(customer.getClass());
+		System.out.println(customer.getCustomerId());
+		System.out.println(customer.getCustomerName());
+	}
+}

617c6fbccd91b652dd7eb72fa01ee73ff8bd9483
NyaNyaXin <chenxin95013>
2017-04-08 22:23:58 +0800

    Spring整合Hibernate之配置文件

---
+++ Spring04/src/com/cx/spring/hibernate/entities/Account.java
@@ -0,0 +1,52 @@
+package com.cx.spring.hibernate.entities;
+
+public class Account {
+	private Integer id;
+	private String username;
+	private int balance;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the balance
+	 */
+	public int getBalance() {
+		return balance;
+	}
+
+	/**
+	 * @param balance
+	 *            the balance to set
+	 */
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+}
---
+++ Spring04/src/com/cx/spring/hibernate/entities/Book.java
@@ -0,0 +1,81 @@
+package com.cx.spring.hibernate.entities;
+
+public class Book {
+	private Integer id;
+	private String bookName;
+	private String isbn;
+	private int price;
+	private int stock;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the bookName
+	 */
+	public String getBookName() {
+		return bookName;
+	}
+
+	/**
+	 * @param bookName
+	 *            the bookName to set
+	 */
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public int getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price
+	 *            the price to set
+	 */
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the stock
+	 */
+	public int getStock() {
+		return stock;
+	}
+
+	/**
+	 * @param stock
+	 *            the stock to set
+	 */
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	/**
+	 * @param isbn
+	 *            the isbn to set
+	 */
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+}

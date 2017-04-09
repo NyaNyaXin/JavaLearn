16cc2b5907935671fca835622723147c51b83c39
NyaNyaXin <chenxin95013>
2017-04-09 20:40:41 +0800

    Spring4整合Struts2

---
+++ Spring06/src/com/cx/spring/struts2/actions/PersonAction.java
@@ -0,0 +1,18 @@
+package com.cx.spring.struts2.actions;
+
+import com.cx.spring.struts2.services.PersonService;
+
+public class PersonAction {
+
+	private PersonService personService;
+
+	public void setPersonService(PersonService personService) {
+		this.personService = personService;
+	}
+
+	public String execute() {
+		System.out.println("execute.......");
+		personService.save();
+		return "success";
+	}
+}
---
+++ Spring06/src/com/cx/spring/struts2/services/PersonService.java
@@ -0,0 +1,7 @@
+package com.cx.spring.struts2.services;
+
+public class PersonService {
+	public void save(){
+		System.out.println("PersonService Save.....");
+	}
+}

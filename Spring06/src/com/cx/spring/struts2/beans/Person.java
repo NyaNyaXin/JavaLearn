1d5dd45264f0a3400cc478a8cfbbf4239cd713b5
NyaNyaXin <chenxin95013>
2017-04-09 19:23:33 +0800

    在Web应用中使用Spring的步骤

---
+++ Spring06/src/com/cx/spring/struts2/beans/Person.java
@@ -0,0 +1,12 @@
+package com.cx.spring.struts2.beans;
+
+public class Person {
+	private String username;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public void hello(){
+		System.out.println("My name is" + username);
+	}
+}

4e3fe604484f316832d672166b05bb823658dcca
NyaNyaXin <chenxin95013>
2017-04-16 14:43:38 +0800

    Java8新特性StreamAPI之终止操作(归约与收集)

--- What'sNewOfJava8/src/com/cx/java8/stream/TestStreamAPI3.java
+++ What'sNewOfJava8/src/com/cx/java8/stream/TestStreamAPI3.java
@@ -4,8 +4,13 @@ package com.cx.java8.stream;
  * */
 
 import java.util.Arrays;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -38,16 +43,116 @@ public class TestStreamAPI3 {
 		Optional<Employee> op = employees.stream().sorted((e1, e2) -> Double.compare(e1.getSalary(), e2.getSalary()))
 				.findFirst();
 		System.out.println(op.get());
-		
-		Optional<Employee> op2 = employees.parallelStream().filter((e)->e.getStatus().equals(Status.FREE)).findAny();
+
+		Optional<Employee> op2 = employees.parallelStream().filter((e) -> e.getStatus().equals(Status.FREE)).findAny();
 		System.out.println(op2.get());
-		
+
 		System.out.println(employees.stream().count());
-		
-		Optional<Employee> op3 = employees.stream().max((e1,e2)->Double.compare(e1.getSalary(), e2.getSalary()));
+
+		Optional<Employee> op3 = employees.stream().max((e1, e2) -> Double.compare(e1.getSalary(), e2.getSalary()));
 		System.out.println(op3.get());
-		
+
 		Optional<Double> op4 = employees.stream().map(Employee::getSalary).min(Double::compare);
 		System.out.println(op4.get());
+	}
+
+	/**
+	 * 规约 reduce(T
+	 * identity,BinaryOperator)/reduce(BinaryOperator)--可以将流中元素反复结合起来，得到一个值
+	 */
+	@Test
+	public void test2() {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		Integer sum = list.stream().reduce(0, (x, y) -> x + y);
+		System.out.println(sum);
+		System.out.println("-------------------------------------------");
+		Optional<Double> op = employees.stream().map(Employee::getSalary).reduce(Double::sum);
+		System.out.println(op.get());
+	}
+
+	/*
+	 * 收集： collect--将流转换为其他形式。接收一个Collector接口的实现，用于给Stream中元素做汇总的方法
+	 **/
+	@Test
+	public void test3() {
+		List<String> list = employees.stream().map(Employee::getName).collect(Collectors.toList());
+		list.forEach(System.out::println);
+
+		System.out.println("------------------------------------------");
+		Set<String> set = employees.stream().map(Employee::getName).collect(Collectors.toSet());
+		set.forEach(System.out::println);
+
+		System.out.println("-------------------------------------------");
+		HashSet<String> has = employees.stream().map(Employee::getName).collect(Collectors.toCollection(HashSet::new));
+		has.forEach(System.out::println);
+	}
+
+	@Test
+	public void test4() {
+		// 总数
+		Long count = employees.stream().collect(Collectors.counting());
+		System.out.println(count);
+
+		System.out.println("-------------------------------------------");
+		// 平均值
+		Double avg = employees.stream().collect(Collectors.averagingDouble(Employee::getSalary));
+		System.out.println(avg);
+
+		System.out.println("-------------------------------------------");
+		// 总和
+		Double sum = employees.stream().collect(Collectors.summingDouble(Employee::getSalary));
+		System.out.println(sum);
+		// 最大值
+		System.out.println("-------------------------------------------");
+		Optional<Employee> max = employees.stream()
+				.collect(Collectors.maxBy((e1, e2) -> Double.compare(e1.getSalary(), e2.getSalary())));
+		System.out.println(max.get());
+		// 最小值
+		System.out.println("-------------------------------------------");
+		Optional<Double> min = employees.stream().map(Employee::getSalary).collect(Collectors.minBy(Double::compare));
+		System.out.println(min.get());
+
+	}
+
+	@Test
+	public void test5() {
+		// 分组
+		Map<Status, List<Employee>> map = employees.stream().collect(Collectors.groupingBy(Employee::getStatus));
+		System.out.println(map);
+	}
+
+	@Test
+	public void test6() {
+		// 多级分组
+	 Map<Status, Map<String,List<Employee>>> map =  employees.stream()
+				.collect(Collectors.groupingBy(Employee::getStatus, Collectors.groupingBy((e) -> {
+					if (((Employee)e).getAge() <= 35) {
+						return "青年";
+					} else if (((Employee)e).getAge() <= 50) {
+						return "中年";
+					} else {
+						return "老年";
+					}
+				})));
+	 System.out.println(map);
+	}
+	@Test
+	public void test7(){
+		//分区
+		Map<Boolean, List<Employee>> map = employees.stream().collect(Collectors.partitioningBy((e)->e.getSalary()>950));
+		System.out.println(map);
+	}
+	
+	@Test
+	public void test8(){
+		DoubleSummaryStatistics dss = employees.stream().collect(Collectors.summarizingDouble(Employee::getSalary));
+		System.out.println(dss.getSum());
+		
+	}
+	
+	@Test
+	public void test9(){
+		String s = employees.stream().map(Employee::getName).collect(Collectors.joining(",","===","==="));
+		System.out.println(s);
 	}
 }

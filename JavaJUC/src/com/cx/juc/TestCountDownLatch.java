60ef00186cba1a99c0bb44a372608461986e6b02
NyaNyaXin <chenxin95013>
2017-04-18 16:54:39 +0800

    JUC线程高级之CountDownLatch闭锁

---
+++ JavaJUC/src/com/cx/juc/TestCountDownLatch.java
@@ -0,0 +1,53 @@
+package com.cx.juc;
+
+import java.util.concurrent.CountDownLatch;
+
+/*
+ * 闭锁：
+ * 在完成某些运算时，只有其他所有线程的运算全部完成，当前的运算才会继续执行
+ * **/
+public class TestCountDownLatch {
+	public static void main(String[] args) {
+		final CountDownLatch latch = new CountDownLatch(5);
+		LatchDemo ld = new LatchDemo(latch);
+
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 5; i++) {
+			new Thread(ld).start();
+		}
+
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("耗费时间为：" + (end - start));
+	}
+}
+
+class LatchDemo implements Runnable {
+
+	private CountDownLatch latch;
+
+	public LatchDemo(CountDownLatch latch) {
+		this.latch = latch;
+	}
+
+	@Override
+	public void run() {
+		synchronized (this) {
+			try {
+				for (int i = 0; i < 50000; i++) {
+					if (i % 2 == 0) {
+						System.out.println(i);
+					}
+				}
+			} finally {
+				latch.countDown();
+			}
+		}
+
+	}
+}

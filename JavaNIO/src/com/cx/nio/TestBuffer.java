d4ba87d93d630a0401d5c4b99a3d84a2d1f972d0
NyaNyaXin <chenxin95013>
2017-04-20 19:40:31 +0800

    Java NIO(新IO)之直接缓冲区与非直接缓冲区

--- JavaNIO/src/com/cx/nio/TestBuffer.java
+++ JavaNIO/src/com/cx/nio/TestBuffer.java
@@ -30,8 +30,20 @@ import org.junit.Test;
  * 
  * 0<=mark<=position<=limit<=capacity
  * 
+ * 五、直接缓冲区与非直接缓冲区
+ * 非直接缓冲区：通过allocate()方法分配缓冲区，将缓冲区建立在JVM的内存中
+ * 直接缓冲区：通过allocateDirect()方法分配直接缓冲区，将缓冲区简历在操作系统的物理内存中。可以提高效率
+ * 
  * */
 public class TestBuffer {
+	
+	@Test
+	public void test3(){
+		//分配直接缓冲区
+		ByteBuffer buf = ByteBuffer.allocateDirect(1024);
+		//查看当前缓冲区是否为直接缓冲区
+		System.out.println(buf.isDirect());
+	}
 	
 	@Test
 	public void test2(){
